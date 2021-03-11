aa01c8973411dd7db48d0aa9f746d0f6a532153c
mgwal <mgwalsh@unomaha.edu>
2021-03-11 14:51:23 -0600

    Initial Commit

--- app/src/main/java/com/example/smartchargecapstoneproject/MainMenuActivity.java
+++ app/src/main/java/com/example/smartchargecapstoneproject/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.smartchargecapstoneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,24 +10,66 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainMenuActivity extends AppCompatActivity {
-    Button btnSignOut;
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
     FirebaseAuth fAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    public CardView homeCard, roomsCard, analyticsCard, historyCard, accountCard, settingsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        btnSignOut = findViewById(R.id.btnSignOut);
 
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intToLogin = new Intent(MainMenuActivity.this, LoginActivity.class);
-                startActivity(intToLogin);
-            }
-        });
+        homeCard = (CardView) findViewById(R.id.homeCard);
+        roomsCard = (CardView) findViewById(R.id.roomsCard);
+        analyticsCard = (CardView) findViewById(R.id.analyticsCard);
+        historyCard = (CardView) findViewById(R.id.historyCard);
+        accountCard = (CardView) findViewById(R.id.accountCard);
+        settingsCard = (CardView) findViewById(R.id.settingsCard);
+
+        homeCard.setOnClickListener(this);
+        roomsCard.setOnClickListener(this);
+        analyticsCard.setOnClickListener(this);
+        historyCard.setOnClickListener(this);
+        accountCard.setOnClickListener(this);
+        settingsCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.homeCard :
+                i = new Intent(this, Home.class);
+                startActivity(i);
+                break;
+
+            case R.id.roomsCard :
+                i = new Intent(this, Rooms.class);
+                startActivity(i);
+                break;
+
+            case R.id.analyticsCard :
+                i = new Intent(this, Analytics.class);
+                startActivity(i);
+                break;
+
+            case R.id.historyCard :
+                i = new Intent(this, History.class);
+                startActivity(i);
+                break;
+
+            case R.id.accountCard :
+                i = new Intent(this, Account.class);
+                startActivity(i);
+                break;
+
+            case R.id.settingsCard :
+                i = new Intent(this, Settings.class);
+                startActivity(i);
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/smartchargecapstoneproject/Settings.java
@@ -0,0 +1,33 @@
+package com.example.smartchargecapstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Settings extends AppCompatActivity {
+    Button btnSignOut;
+    FirebaseAuth fAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        btnSignOut = findViewById(R.id.btnSignOut);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToLogin = new Intent(Settings.this, LoginActivity.class);
+                startActivity(intToLogin);
+                finish();
+            }
+        });
+    }
+}
